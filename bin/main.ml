e84c6940d24a7f487ba47cf25677e8eb871fdeb0
Andreas Garnaes <andreas.garnaes@gmail.com>
2019-06-08 14:51:20 +0200

    CLI: Allow specifying path to bushel

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,25 @@
 open Lwt.Infix
 
-let path = "/tmp/bushel-test"
+let bushel =
+  let open Cmdliner in
+  let path =
+    let doc =
+      Arg.info ~doc:"Bushel path" ~docs:"foo"
+        [ "p"; "path" ]
+    in
+    Arg.(value & opt string "." & doc)
+  in
+  let create path =
+    Bushel.v path
+  in
+  Term.(const create $ path)
 
-let run_cmd cmd =
+let run_cmd cmd bushel =
   Lwt_main.run begin
     Lwt.catch
-      cmd
+      (fun () ->
+        bushel >>= cmd
+      )
       (fun exn ->
         Format.printf "ERROR: %s\n%s\n" (Printexc.to_string exn) (Printexc.get_backtrace ());
         Lwt.return_unit
@@ -21,21 +35,20 @@ let default =
 
 let graphql =
   let open Cmdliner in
-  let cmd () =
-    run_cmd (fun () ->
-      Bushel.v path >>= fun bushel ->
+  let cmd =
+    run_cmd (fun bushel ->
       let server = Bushel.Server.v bushel in
-      Conduit_lwt_unix.init ~src:"localhost" () >>= fun ctx ->
+      Conduit_lwt_unix.init ~src:"127.0.0.1" () >>= fun ctx ->
       let ctx = Cohttp_lwt_unix.Net.init ~ctx () in
       let on_exn exn =
-        Logs.debug (fun l -> l "on_exn: %s" (Printexc.to_string exn))
+        Logs.err (fun l -> l "on_exn: %s" (Printexc.to_string exn))
       in
       Cohttp_lwt_unix.Server.create ~on_exn ~ctx
         ~mode:(`TCP (`Port 8888))
         server
     )
   in
-  Term.(const cmd $ const (), info "graphql")
+  Term.(const cmd $ bushel, info "graphql")
 
 let add_repo =
   let open Cmdliner in
@@ -52,12 +65,11 @@ let add_repo =
     Arg.(required & pos 2 (some string) None & info [] ~doc)
   in
   let cmd cookie owner name =
-    run_cmd (fun () ->
-      Bushel.v path >>= fun bushel ->
+    run_cmd (fun bushel ->
       Bushel.add_repo bushel ~cookie ~owner ~name
     )
   in
-  Term.(const cmd $ cookie $ owner $ name_, info "add_repo")
+  Term.(const cmd $ cookie $ owner $ name_ $ bushel, info "add_repo")
 
 let import_post =
   let open Cmdliner in
@@ -70,12 +82,11 @@ let import_post =
     Arg.(required & pos 1 (some string) None & info [] ~doc)
   in
   let cmd site file =
-    run_cmd (fun () ->
-      Bushel.v path >>= fun bushel ->
+    run_cmd (fun bushel ->
       Bushel.import_post bushel ~site ~file
     )
   in
-  Term.(const cmd $ site $ file, info "import_post")
+  Term.(const cmd $ site $ file $ bushel, info "import_post")
 
 let sync =
   let open Cmdliner in
@@ -84,12 +95,11 @@ let sync =
     Arg.(required & pos 0 (some string) None & info [] ~env)
   in
   let cmd cookie =
-    run_cmd (fun () ->
-      Bushel.v path >>= fun bushel ->
+    run_cmd (fun bushel ->
       Bushel.sync bushel ~cookie
     )
   in
-  Term.(const cmd $ cookie, info "sync")
+  Term.(const cmd $ cookie $ bushel, info "sync")
 
 let commands = [
   graphql;
