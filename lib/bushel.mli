81052335e4f1ce97d2388abac773d90bd886bb48
Anil Madhavapeddy <anil@recoil.org>
2025-02-01 15:06:48 +0000

    iface

---
+++ lib/bushel.mli
@@ -0,0 +1,10 @@
+(** Bushel *)
+
+module Contact = Contact
+module Idea = Idea
+module News = News
+module Paper = Paper
+module Project = Project
+module Video = Video
+module Tags = Tags
+module Entry = Entry
