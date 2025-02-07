cdc0df0f7574ac5ac7b83d6767c64ed29bf1f158
Anil Madhavapeddy <anil@recoil.org>
2025-02-07 18:00:45 +0000

    more paper work

--- bin/bushel_paper.ml
+++ bin/bushel_paper.ml
@@ -1,1 +1,16 @@
-module Z = Zotero_translation
+module ZT = Zotero_translation
+open Lwt.Infix
+
+let print_json j =
+  print_endline (Ezjsonm.to_string j)
+
+let of_doi zt doi =
+  ZT.json_of_doi zt doi  >>= fun j ->
+  print_json j;
+  Lwt.return ()
+
+let () =
+  (* TODO cmdliner *)
+  let doi = Sys.argv.(1) in
+  let zt = ZT.v "http://svr-avsm2-eeg-ce:1969" in
+  Lwt_main.run @@ of_doi zt doi
