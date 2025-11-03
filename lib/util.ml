1f7c470919f8ead6e1ec439e042d7d3bf3e47d00
Anil Madhavapeddy <anil@recoil.org>
2025-11-03 12:55:11 +0000

    more

--- lib/util.ml
+++ lib/util.ml
@@ -19,6 +19,41 @@ let first_and_last_hunks s =
   aux [] lines
 ;;
 
+(* Find all footnote definition lines in text *)
+let find_footnote_lines s =
+  let lines = String.split_on_char '\n' s in
+  let is_footnote_def line =
+    String.length line > 3 &&
+    line.[0] = '[' &&
+    line.[1] = '^' &&
+    String.contains line ':' &&
+    let colon_pos = String.index line ':' in
+    colon_pos > 2 && line.[colon_pos - 1] = ']'
+  in
+  let is_continuation line =
+    String.length line > 0 && (line.[0] = ' ' || line.[0] = '\t')
+  in
+  let rec collect_footnotes acc in_footnote = function
+    | [] -> List.rev acc
+    | line :: rest ->
+      if is_footnote_def line then
+        collect_footnotes (line :: acc) true rest
+      else if in_footnote && is_continuation line then
+        collect_footnotes (line :: acc) true rest
+      else
+        collect_footnotes acc false rest
+  in
+  collect_footnotes [] false lines
+;;
+
+(* Augment first hunk with footnote definitions from last hunk *)
+let first_hunk_with_footnotes s =
+  let first, last = first_and_last_hunks s in
+  let footnote_lines = find_footnote_lines last in
+  if footnote_lines = [] then first
+  else first ^ "\n\n" ^ String.concat "\n" footnote_lines
+;;
+
 let count_words (text : string) : int =
   let len = String.length text in
   let rec count_words_helper (index : int) (in_word : bool) (count : int) : int =
